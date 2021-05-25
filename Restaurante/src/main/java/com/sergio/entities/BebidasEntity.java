609e88020dda62cd174ce7d022ea7d146df8769d
SergioZ <serzupo@gmail.com>
2021-05-25 18:15:23 +0200

    Clase Entity de Bebidas

---
+++ Restaurante/src/main/java/com/sergio/entities/BebidasEntity.java
@@ -0,0 +1,64 @@
+package com.sergio.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bebidas")
+public class BebidasEntity {
+
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "precio")
+	private int precio;
+
+	public BebidasEntity() {
+		super();
+	}
+
+	public BebidasEntity(int id, String nombre, int precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public BebidasEntity(String nombre, int precio) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	
+}
