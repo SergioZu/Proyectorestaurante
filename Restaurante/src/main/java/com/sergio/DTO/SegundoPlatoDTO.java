effdda4c1e9500f8984b55d90383e1a7f0085052
SergioZ <serzupo@gmail.com>
2021-05-25 18:46:46 +0200

    Clase DTO Segundo Plato

---
+++ Restaurante/src/main/java/com/sergio/DTO/SegundoPlatoDTO.java
@@ -0,0 +1,42 @@
+package com.sergio.DTO;
+
+public class SegundoPlatoDTO {
+	
+	private int id;
+	private String nombre;
+	private Integer precio;
+	
+	public SegundoPlatoDTO(int id, String nombre, Integer precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+	
+	
+
+}
