cdd0befa8f2f9e11b16521ab9b80e87cb884b172
SergioZ <serzupo@gmail.com>
2021-05-25 18:50:24 +0200

    Clase DTO Salas Disponibles

---
+++ Restaurante/src/main/java/com/sergio/DTO/SalasDisponibleDTO.java
@@ -0,0 +1,32 @@
+package com.sergio.DTO;
+
+public class SalasDisponibleDTO {
+	
+	private int id;
+	private String nombre;
+	
+	public SalasDisponibleDTO(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
