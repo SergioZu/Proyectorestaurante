b3aa96a79f1317ca396b21f71666ac1ba9488ff8
SergioZ <serzupo@gmail.com>
2021-05-25 18:01:50 +0200

    Clase Entity del Primer Plato

--- Restaurante/src/main/java/com/sergio/entities/PrimerPlatoEntity.java
+++ Restaurante/src/main/java/com/sergio/entities/PrimerPlatoEntity.java
@@ -1,5 +1,65 @@
 package com.sergio.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "platoprincipal")
 public class PrimerPlatoEntity {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "precio")
+	private int precio;
+
+	public PrimerPlatoEntity() {
+		super();
+	}
+
+	public PrimerPlatoEntity(int id, String nombre, int precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public PrimerPlatoEntity(String nombre, int precio) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	
 
 }
