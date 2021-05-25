d981062ccd7b2670c5c9ab7645df33886ff71821
SergioZ <serzupo@gmail.com>
2021-05-25 20:17:05 +0200

    Interfaz para los metodos de los postres

--- Restaurante/src/main/java/com/sergio/restaurante/DAO/PostreDAO.java
+++ Restaurante/src/main/java/com/sergio/restaurante/DAO/PostreDAO.java
@@ -2,13 +2,14 @@ package com.sergio.restaurante.DAO;
 
 import java.util.List;
 
-import com.sergio.restaurante.DTO.EntranteDTO;
 
+import com.sergio.restaurante.DTO.PostreDTO;
 
-public interface EntranteDAO {
-
-	List<EntranteDTO> visualizarBebidas(int id, String nombre, int precio);
+public interface PostreDAO {
+	
+	List<PostreDTO> visualizarBebidas(int id, String nombre, int precio);
 	Integer insertarNuevaBebida(int id,String nombre,int precio);
 	Integer modificarBebida(int id,String nombre,int precio);
 	Integer eliminarBebida(int id);
+
 }
