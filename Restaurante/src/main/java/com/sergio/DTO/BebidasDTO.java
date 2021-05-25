365d363fbf50ecf4d390b223c4d9f828767e867c
SergioZ <serzupo@gmail.com>
2021-05-25 18:34:35 +0200

    Clase DTO Bebidas

---
+++ Restaurante/src/main/java/com/sergio/DTO/BebidasDTO.java
@@ -0,0 +1,43 @@
+package com.sergio.DTO;
+
+public class BebidasDTO {
+	private int id;
+	private String nombre;
+	private Integer precio;
+	
+	public BebidasDTO(int id, String nombre, Integer precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+	
+	
+
+}
