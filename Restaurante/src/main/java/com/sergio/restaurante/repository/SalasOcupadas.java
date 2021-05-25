c2bb476d81b646b9c3140856f02c309098beba7f
SergioZ <serzupo@gmail.com>
2021-05-25 19:56:53 +0200

    Interfaz para las Salas Ocupadas

---
+++ Restaurante/src/main/java/com/sergio/restaurante/repository/SalasOcupadas.java
@@ -0,0 +1,27 @@
+package com.sergio.restaurante.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.sergio.restaurante.DTO.SalasOcupadasDTO;
+import com.sergio.restaurante.entities.SalasOcupadasEntity;
+
+
+
+@Repository
+public interface SalasOcupadas extends CrudRepository<SalasOcupadasEntity, Integer>{
+
+	@Query(value = "select new com.sergio.restaurante.dtos.SalasOcupadasDTO (so.id,so.nombre,so.nombreOcupante) "
+			+ "FROM com.sergio.colegio.entities.SalasOcupadasEntity so "
+			+ "WHERE (so.id LIKE CONCAT('%',:id,'%') or :id is null)"
+			+ "AND so.nombre LIKE CONCAT ('%',:nombre,'%')"
+			+ "AND so.nombre LIKE CONCAT ('%',:nombreOcupante,'%')")
+	 		List<SalasOcupadasDTO> obtenerRolPorUsernamePassword(
+					  @Param("id") String id,
+					  @Param("nombre") String nombre,
+					  @Param("nombreOcupante") String nombreOcupante);
+}
