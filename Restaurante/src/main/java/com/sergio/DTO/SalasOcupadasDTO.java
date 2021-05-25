517262351f99ef0959b81c435009a8558629ac69
SergioZ <serzupo@gmail.com>
2021-05-25 18:50:58 +0200

    Clase DTO Salas Ocupante

---
+++ Restaurante/src/main/java/com/sergio/DTO/SalasOcupadasDTO.java
@@ -0,0 +1,41 @@
+package com.sergio.DTO;
+
+public class SalasOcupadasDTO {
+
+	private int id;
+	private String nombre;
+	private String nombreOcupante;
+	
+	public SalasOcupadasDTO(int id, String nombre, String nombreOcupante) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.nombreOcupante = nombreOcupante;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombreOcupante() {
+		return nombreOcupante;
+	}
+
+	public void setNombreOcupante(String nombreOcupante) {
+		this.nombreOcupante = nombreOcupante;
+	}
+	
+	
+}
