27b07100b395b518f85b57e51d90841f18fe42ec
SergioZ <serzupo@gmail.com>
2021-05-29 12:12:31 +0200

    Creacion de la clase BebidaDAOimpl

---
+++ Restaurante/src/main/java/com/sergio/restaurante/DAO/Impl/BebidaDAOImpl.java
@@ -0,0 +1,37 @@
+package com.sergio.restaurante.DAO.Impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sergio.restaurante.DAO.BebidasDAO;
+import com.sergio.restaurante.DTO.BebidasDTO;
+
+@Service
+public class BebidaDAOImpl implements BebidasDAO{
+
+	@Override
+	public List<BebidasDTO> visualizarBebidas(int id, String nombre, int precio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer insertarNuevaBebida(int id, String nombre, int precio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer modificarBebida(int id, String nombre, int precio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer eliminarBebida(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
