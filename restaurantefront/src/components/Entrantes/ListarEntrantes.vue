e3bd05c928be55d17dd8c42986fc3ba73f470a9a
SergioZ <serzupo@gmail.com>
2021-06-08 16:27:36 +0200

    Componente para listar los entrantes

---
+++ restaurantefront/src/components/Entrantes/ListarEntrantes.vue
@@ -0,0 +1,59 @@
+<template>
+ <table style="width:100%">
+      <tr>
+        <th>id</th>
+        <th>Nombre</th>
+        <th>Precios</th> 
+      </tr>
+       <tr v-for="entrante in entrantes" :key="entrante">
+          <td>{{ entrante.id }}</td>
+          <td>{{ entrante.nombre }}</td>
+           <td>{{ entrante.precio }}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  data() {
+        return {
+            entrantes: []
+        }
+    },
+  methods:{
+    obtenerBebidas: function(){
+      axios
+        .get("http://localhost:8080/restaurante/v1/entrante")
+        .then(response => {
+          this.entrantes = response.data;
+        })
+        .catch(response=>alert("Error al recuperar datos "+response.status));
+      }
+  },
+   created(){
+     this.obtenerBebidas();
+    }
+    
+}
+
+</script>
+
+<style scoped>
+
+table{
+   position: absolute;
+}
+
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+ 
+}
+th, td {
+  padding: 15px;
+}
+
+</style>
---
+++ restaurantefront/src/views/Entrante.vue
@@ -0,0 +1,31 @@
+<template>
+ 
+  <div class="clasificacion">
+     <h2> Entrantes:</h2>
+    <ListarEntrantes/>
+  </div>
+</template>
+
+<script>
+import ListarEntrantes from '@/components/Entrantes/ListarEntrantes.vue'
+
+export default {
+   
+    components: {
+    ListarEntrantes
+  }
+}
+</script>
+
+
+<style scoped>
+
+.clasificacion{
+  width: 30%;
+  margin-left: 20%;
+  position: absolute;
+ 
+}
+
+
+</style>
