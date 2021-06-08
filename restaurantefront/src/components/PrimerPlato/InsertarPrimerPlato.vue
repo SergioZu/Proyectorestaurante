5731b3e80e89174a51f66c632d0bb973d0e20061
SergioZ <serzupo@gmail.com>
2021-06-08 17:37:59 +0200

    Componente para Insertar Primer Plato

---
+++ restaurantefront/src/components/PrimerPlato/InsertarPrimerPlato.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class="nuevaBebidas">
+      <h2>Insertar</h2>
+    <form>
+      <label >Nombre del Primer Plato:</label><br>
+      <input type="text"  v-model="nombrePrimerPlato"><br>
+      <label >Precio del Primer Plato:</label><br>
+      <input type="number" v-model="precioPrimerPlato"><br>
+      <button type="button"  @click="crearBebida()">enviar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+        return {
+            nombrePrimerPlato:"",
+            precioPrimerPlato:""
+        }
+    },
+  
+    methods:{
+      crearBebida: function(){
+        let primerplatos={
+          nombre:this.nombrePrimerPlato,
+          precio:this.precioPrimerPlato,
+        }
+        axios.post("http://localhost:8080/restaurante/v1/primerplato", primerplatos).then((result) => {
+            alert("Se ha insertado la Bebida Correctamente");
+            });
+        }
+    }
+  
+}
+</script>
+
+<style>
+
+.nuevaBebidas{
+  width: 100%;
+  position: absolute;
+  top:500px;
+}
+
+</style>
