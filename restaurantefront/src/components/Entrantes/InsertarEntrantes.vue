0c9873350e025ae1bb46a3c68612b2ec057133f0
SergioZ <serzupo@gmail.com>
2021-06-08 16:32:52 +0200

    Componente para Insertar Entrantes

---
+++ restaurantefront/src/components/Entrantes/InsertarEntrantes.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class="nuevaBebidas">
+      <h2>Insertar</h2>
+    <form>
+      <label >Nombre del Entrante:</label><br>
+      <input type="text"  v-model="nombreEntrante"><br>
+      <label >Precio del Entrante:</label><br>
+      <input type="number" v-model="precioEntrante"><br>
+      <button type="button"  @click="crearBebida()">enviar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+        return {
+            nombreEntrante:"",
+            precioEntrante:""
+        }
+    },
+  
+    methods:{
+      crearBebida: function(){
+        let entrantes={
+          nombre:this.nombreEntrante,
+          precio:this.precioEntrante,
+        }
+        axios.post("http://localhost:8080/restaurante/v1/entrante", entrantes).then((result) => {
+            alert("Se ha insertado la Bebida Correctamente");
+            });
+        }
+    }
+  
+}
+</script>
+
+<style>
+
+.nuevaBebidas{
+  width: 100%;
+  position: absolute;
+  top:500px;
+}
+
+</style>
--- restaurantefront/src/views/Entrante.vue
+++ restaurantefront/src/views/Entrante.vue
@@ -3,16 +3,19 @@
   <div class="clasificacion">
      <h2> Entrantes:</h2>
     <ListarEntrantes/>
+    <InsertarEntrantes/>
   </div>
 </template>
 
 <script>
 import ListarEntrantes from '@/components/Entrantes/ListarEntrantes.vue'
+import InsertarEntrantes from '@/components/Entrantes/InsertarEntrantes.vue'
 
 export default {
    
     components: {
-    ListarEntrantes
+    ListarEntrantes,
+    InsertarEntrantes
   }
 }
 </script>
