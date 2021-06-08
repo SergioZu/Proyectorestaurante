21325eed4bbde3a38226e42d21abb315d8272a5a
SergioZ <serzupo@gmail.com>
2021-06-08 16:47:27 +0200

    Vista para las Entrantes

--- restaurantefront/src/views/Entrante.vue
+++ restaurantefront/src/views/Entrante.vue
@@ -4,18 +4,24 @@
      <h2> Entrantes:</h2>
     <ListarEntrantes/>
     <InsertarEntrantes/>
+    <ModificarEntrantes/>
+    <EliminarEntrantes/>
   </div>
 </template>
 
 <script>
 import ListarEntrantes from '@/components/Entrantes/ListarEntrantes.vue'
 import InsertarEntrantes from '@/components/Entrantes/InsertarEntrantes.vue'
+import ModificarEntrantes from '@/components/Entrantes/ModificarEntrantes.vue'
+import EliminarEntrantes from '@/components/Entrantes/EliminarEntrantes.vue'
 
 export default {
    
     components: {
     ListarEntrantes,
-    InsertarEntrantes
+    InsertarEntrantes,
+    ModificarEntrantes,
+    EliminarEntrantes
   }
 }
 </script>
